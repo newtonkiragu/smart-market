945dd18c09d8f200ad209ce097a0ce4a8e25dadb
newtonkiragu <karanunewton4@gmail.com>
2017-03-07 23:56:07 +0300

    added the first of js

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+var wholesale = "once a month"
+var retail = "twice a month"
+var consumer = "four times a month"
+
+function options(wholesale, retail, consumer) {
+  this.wholesale = wholesale;
+  this.retail = retail;
+  this.consumer = consumer;
+}
+
+function items(kales, onions, tomatoes, potatoes, bananas, apples, mangoes, oranges, beef, chicken, fish, minced, passion, mango, orange, smoothie) {
+  this.kales = kale;
+  this.onions = onions;
+  this.tomatoes = tomatoes;
+  this.potatoes = potatoes;
+  this.bananas = bananas;
+  this.apples = apples;
+  this.mangoes = mangoes;
+  this.oranges = oranges;
+  this.beef = beef;
+  this.chicken = chicken;
+  this.fish = fish;
+  this.minced = mimced;
+  this.passion = passion;
+  this.mango = mango;
+  this.orange = orange;
+  this.smoothie = smoothie;
+}
+// returns the names of the items as  outputs
+Options.prototype.itemName = function() {
+  return items;
+}
