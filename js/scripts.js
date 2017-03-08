53f8e980a2ae3d1121622b0147898cc56b19a04b
newtonkiragu <karanunewton4@gmail.com>
2017-03-08 16:01:22 +0300

    added navbar to everything

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,20 @@ $(document).ready(function() {
     $(".quantity").text(kale);
   });
 });
+
+// IDEA: code from index and contacts
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    var input = $("#userinfo").val();
+    var greeting = function() {
+      alert("Thank you " + input + " for logging in. Hope you enjoy.")
+    };
+    var hide = function() {
+      ("#tohideform").hide();
+    };
+    var show = function() {
+      ("#userinfo").show();
+    };
+    (".btn").click(greeting());
+  });
+});
